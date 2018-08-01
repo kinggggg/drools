21d9364e5752dac7cdb39a14d542ff492d326152
zeek <liweibooooo@126.com>
2018-08-01 16:59:20 +0800

    基本比较，逻辑运算符测试

--- drools-lesson/src/main/java/com/secbro/drools/chapter0/LearnDroolsTest.java
+++ drools-lesson/src/main/java/com/secbro/drools/chapter0/LearnDroolsTest.java
@@ -10,6 +10,258 @@ import org.kie.api.runtime.KieSession;
  */
 public class LearnDroolsTest extends BaseTest{
 
+    /**
+     *  not in 测试
+     */
+    @Test
+    public void notInTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName("zhaoliu");
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  in 测试
+     */
+    @Test
+    public void inTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName("wangwu");
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  != null 测试
+     */
+    @Test
+    public void neNullTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName("wangwu");
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  == null 测试
+     */
+    @Test
+    public void eNullTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName(null);
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  not soundslike 测试
+     */
+    @Test
+    public void notSoundsLikeTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName("word");
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  soundslike 测试
+     */
+    @Test
+    public void soundsLikeTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName("world");
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  not matches 测试
+     */
+    @Test
+    public void notMatchesTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName("李四");
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  matches 测试
+     */
+    @Test
+    public void matchesTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setName("张三");
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  != 测试
+     */
+    @Test
+    public void neTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setAge(19);
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  == 测试
+     */
+    @Test
+    public void eTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setAge(18);
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  <= 测试
+     */
+    @Test
+    public void leTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setAge(17);
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  < 测试
+     */
+    @Test
+    public void lTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setAge(17);
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  >= 测试
+     */
+    @Test
+    public void geTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setAge(18);
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
+    /**
+     *  > 测试
+     */
+    @Test
+    public void gTest(){
+        KieSession kieSession = getKieSessionBySessionName("learn-drools");
+
+        Person p1 = new Person();
+        p1.setAge(20);
+
+        kieSession.insert(p1);
+
+        int count = kieSession.fireAllRules();
+        kieSession.dispose();
+
+        System.out.println("Fire " + count + " rules!");
+    }
+
     @Test
     public void nullSafeTest(){
         KieSession kieSession = getKieSessionBySessionName("learn-drools");
